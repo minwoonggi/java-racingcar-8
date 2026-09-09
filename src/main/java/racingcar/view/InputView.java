87c64a0b381082c02bd5023abd120d329e99935a
minwoonggi <d34sda334@gmail.com>
2026-09-09 20:28:22 +0900

    feat(input): 시도 횟수 입력값 검증 로직 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,5 @@
 package racingcar.view;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
@@ -9,23 +8,41 @@ public class InputView {
         return List.of(input.split(",",-1));
     }
 
-    public static void isValidInput(List<String> names){
-        validateEmptyName(names);
-        validateDuplicateName(names);
+    public static void isValidCarNameInput(List<String> names){
+        validateEmptyCarName(names);
+        validateDuplicateCarName(names);
     }
 
-    private static void validateEmptyName(List<String> names){
+    public static void isValidAttemptCount(String input){
+        try{
+            validateIntegerAttemptCount(input);
+            validatePositiveAttemptCount(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("정수값을 입력받아야 합니다.");
+        }
+    }
+
+    private static void validateEmptyCarName(List<String> names){
         if (names.stream().anyMatch(String::isBlank))
             throw new IllegalArgumentException("비어있는 입력값이 존재합니다.");
     }
 
-    private static void validateDuplicateName(List<String> names){
+    private static void validateDuplicateCarName(List<String> names){
         long uniqueCount = names.stream()
                 .distinct()
                 .count();
 
         if (uniqueCount != names.size())
             throw new IllegalArgumentException("중복된 입력값이 존재합니다.");
+    }
+    private static void validateIntegerAttemptCount(String input){
+       Integer.parseInt(input);
+    }
+
+    private static void validatePositiveAttemptCount(String input){
+        int num = Integer.parseInt(input);
+        if (num<0)
+            throw new IllegalArgumentException("0보다 큰값을 입력받아야 합니다.");
     }
 
 }
--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -33,23 +33,45 @@ public class InputViewTest {
     }
 
     @Nested
-    class 입력값_검증{
+    class 자동차_이름_입력값_검증{
         @Test
-        void 비어있는_입력값(){
+        void 비어있는_자동차_이름_입력값(){
             List<String> names = List.of("pobi","woni"," ");
 
-            assertThatThrownBy(() -> InputView.isValidInput(names))
+            assertThatThrownBy(() -> InputView.isValidCarNameInput(names))
                    .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("비어있는 입력값이 존재합니다.");
         }
 
         @Test
-        void 중복된_입력값(){
+        void 중복된_자동차_이름_입력값(){
             List<String> names = List.of("pobi","woni","pobi");
 
-            assertThatThrownBy(() -> InputView.isValidInput(names))
+            assertThatThrownBy(() -> InputView.isValidCarNameInput(names))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("중복된 입력값이 존재합니다.");
         }
+    }
+
+    @Nested
+    class 시도할_횟수_검증{
+        @Test
+        void 정수가_아닌_입력값(){
+            String input = "문자";
+
+            assertThatThrownBy(()->InputView.isValidAttemptCount(input))
+                    .isExactlyInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("정수값을 입력받아야 합니다.");
+        }
+
+        @Test
+        void 음수_입력값(){
+            String input = "-1";
+
+            assertThatThrownBy(()->InputView.isValidAttemptCount(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("0보다 큰값을 입력받아야 합니다.");
+        }
+
     }
 }
